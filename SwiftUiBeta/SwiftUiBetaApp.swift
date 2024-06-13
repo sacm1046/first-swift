66d488e235aee98e31cc04846ca07ff59148164b
sacm <sacm1046@gmail.com>
2024-06-12 23:49:42 -0400

    Initial Commit

---
+++ SwiftUiBeta/SwiftUiBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBetaApp.swift
+//  SwiftUiBeta
+//
+//  Created by Seba on 12-06-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
